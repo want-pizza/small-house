32dd03d564b749fa7a5a1f51c8234b3aa7d210df
want-pizza <olex2005my@gmail.com>
2024-05-07 16:07:08 +0300

    added tilemap
    animations, update camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] float followSpeed = 2f;
     [SerializeField] float mouseSensitivity = 2f;
     [SerializeField] float aimDistanceMult = 2f;
+    [SerializeField] float aimSpeed = 0.01f;
+    [SerializeField] float maxDistanceFromPlayer = 5f;
+    private bool returning = false;
 
     private Vector3 offset;
 
@@ -21,19 +25,54 @@ public class CameraController : MonoBehaviour
         {
             Aiming();
         }
-        else
-        {
-            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z) + offset;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
-        }
+        else FollowPlayer();
     }
 
     private void Aiming()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        if (returning) 
+        {
+            FollowPlayer(0.3f);
+            return;
+        }
 
-        transform.Translate(Vector3.right * aimDistanceMult * mouseX * Time.deltaTime);
-        transform.Translate(Vector3.up * aimDistanceMult * mouseY * Time.deltaTime);
+        Vector3 mousePos = Input.mousePosition; 
+
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        float screenEdgeDistance = 1f; 
+
+        Vector3 direction = (targetPos - transform.position).normalized;
+
+        Vector3 newPosition = transform.position + direction * Mathf.Min((Camera.main.orthographicSize * Camera.main.aspect) - screenEdgeDistance,
+            (targetPos - transform.position).magnitude);
+        newPosition.z = -10;
+
+        float distanceFromPlayer = Vector3.Distance(transform.position, new Vector3(player.position.x, player.position.y, -10));
+        if (distanceFromPlayer < maxDistanceFromPlayer)
+        {
+            transform.position = Vector3.Lerp(transform.position, newPosition, aimSpeed * Time.deltaTime);
+        }
+        else 
+        { 
+            returning = true;
+            StartCoroutine(SetReturningFalse(1f)); 
+        }
     }
+
+    IEnumerator SetReturningFalse(float time)
+    {
+        yield return new WaitForSeconds(time);
+        returning = false;
+    }
+
+    private void FollowPlayer(float speed = 2f)
+    {
+        if (transform.position != new Vector3(player.position.x, player.position.y, -10))
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, 0) + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+    }
+
 }
